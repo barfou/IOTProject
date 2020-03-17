567ab3b2ec924650890a7cc1259e5d5c3d0c1317
PaulFgx <pfourgoux@live.fr>
2020-03-17 17:30:32 +0100

    add salle view holder

--- app/src/main/java/fr/barfou/ui/widget/viewholder/SalleViewHolder.kt
+++ app/src/main/java/fr/barfou/ui/widget/viewholder/SalleViewHolder.kt
@@ -1,2 +1,53 @@
 package fr.barfou.ui.widget.viewholder
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import fr.barfou.data.model.Salle
+
+/**
+ * SAM (Single Abstract Method) to listen a click.
+ *
+ * This callback contains the view clicked, and the character attached to the view
+ */
+typealias OnUserClickListener = (view: View, salle: Salle, type: ClickType) -> Unit
+
+enum class ClickType {
+    NORMAL,
+    LONG
+}
+
+class SalleViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(model: Salle, onClick: OnUserClickListener) {
+        itemView.apply {
+            this.setOnClickListener { onClick(it, model, ClickType.NORMAL) }
+            this.setOnLongClickListener {
+                onClick(it, model, ClickType.LONG)
+                true
+            }
+            this.holder_user_login.text = model.login
+            this.holder_user_type.text = model.type
+            this.holder_user_site_admin.isChecked = model.site_admin
+            Glide.with(this)
+                .load(model.avatar_url)
+                .into(this.holder_user_avatar)
+        }
+    }
+
+    companion object {
+        /**
+         * Create a new Instance of [UserViewHolder]
+         */
+        fun create(parent: ViewGroup): UserViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(
+                R.layout.holder_user,
+                parent,
+                false
+            )
+            return UserViewHolder(view)
+        }
+    }
+}
