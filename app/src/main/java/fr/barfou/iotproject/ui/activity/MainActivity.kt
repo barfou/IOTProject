6d5ba94deb80982493c791e0f0cc1ee81161cb1a
PaulFgx <pfourgoux@live.fr>
2020-04-09 08:33:59 +0200

    setup firebase ok

--- app/src/main/java/fr/barfou/iotproject/ui/activity/MainActivity.kt
+++ app/src/main/java/fr/barfou/iotproject/ui/activity/MainActivity.kt
@@ -2,7 +2,14 @@ package fr.barfou.iotproject.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.findNavController
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import fr.barfou.iotproject.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initToolBar()
+        //testFirebase()
     }
 
     override fun onNavigateUp(): Boolean {
@@ -23,5 +31,28 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(tool_bar)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
         tool_bar.setNavigationOnClickListener { onNavigateUp() }
+    }
+
+    private fun testFirebase() {
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d("TestFirebase", "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("TestFirebase", "Failed to read value.", error.toException())
+            }
+        })
     }
 }
